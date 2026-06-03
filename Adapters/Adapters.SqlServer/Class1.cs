b581c3ec04cfa923164294983c5cc3a144d89c15
wgabrielribeiro <ogabrielribeiro97@gmail.com>
2026-06-03 16:52:34 -0300

    Adicionando transação de inserção de ordem no adapter sql server.

--- Adapters/Adapters.SqlServer/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Adapters.SqlServer;
-
-public class Class1
-{
-}
--- Adapters/Adapters.SqlServer/OrderCommandRepository.cs
+++ Adapters/Adapters.SqlServer/OrderCommandRepository.cs
@@ -1,5 +1,7 @@
+using Adapters.SqlServer.Utils;
 using Core.Application.Repositorio.Commands;
 using Core.Domain.Entities;
+using Dapper;
 using Microsoft.Data.SqlClient;
 
 namespace Adapters.SqlServer;
@@ -7,17 +9,36 @@ namespace Adapters.SqlServer;
 public class OrderCommandRepository : IOrderCommandsRepository
 {
 
-    public void PlaceOrder(Customer costumer, Order order)
+    public void PlaceOrder(Customer customer, Order order)
     {
         try
         {
-            using var dbConnection = new SqlConnection("");
+            using var dbConnection = new SqlConnection(SqlConnectionStringUtils.ConnectionString);
+            dbConnection.Open();
+
+            var newOrder = new
+            {
+                id = order.Id,
+                customer_name = customer.Name.FirstName + " " + customer.Name.LastName,
+                customer_cpf = customer.Cpf,
+                customer_email = customer.Email,
+                customer_phone = customer.Phone,
+                customer_id = customer.Id,
+                order_date = order.CreationDate,
+                order_status = order.Status.ToString()
+            };
+
+            dbConnection.Execute(
+                "INSERT INTO [dbo].[orders] ([id], [customer_name], [customer_cpf], [customer_email], [costumer_phone], [costumer_id], [order_date], [order_status])" +
+                "\n VALUES(@id, @customer_name, @customer_cpf, @customer_email, @costumer_phone, @costumer_id, @order_date, @order_status)",
+                newOrder);
+            
+            dbConnection.Close();
 
         }
         catch (Exception e)
         {
-            Console.WriteLine(e);
-            throw;
+            throw new Exception("OrdersRepository --> PlaceOrder: Erro: Falha ao criar ordem. " + e.Message, e);
         }
     }
 }
